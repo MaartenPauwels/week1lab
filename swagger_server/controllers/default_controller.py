14bf8fc337139331348ac5b1d74add6feab131fc
Maarten <maarten-spam@hotmail.com>
2023-02-08 19:45:44 +0100

    test

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -19,7 +19,6 @@ def add_student(body=None):  # noqa: E501
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
         return add(body)
-        #test
     return 500, 'error'
 
 
